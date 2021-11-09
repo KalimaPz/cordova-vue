df8813333f2a77b92e05bee769f1e93874959944
krit97s <jakkrit.ss@hotmail.com>
2021-11-10 02:23:57 +0700

    .

--- src/router/index.js
+++ src/router/index.js
@@ -1,49 +1,56 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
-import Register from '../views/Register.vue'
-import View from '../views/View.vue' 
-import Adress from '../views/Address.vue' 
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import View from "../views/View.vue";
+import Adress from "../views/Address.vue";
+import Cart from "../views/Cart.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/address',
-    name: 'Address',
-    component: Adress
+    path: "/address",
+    name: "Address",
+    component: Adress,
   },
   {
-    path: '/view',
-    name: 'View',
-    component: View
+    path: "/cart",
+    name: "Cart",
+    component: Cart,
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
+    path: "/view",
+    name: "View",
+    component: View,
   },
   {
-    path: '/register',
-    name: 'Register',
-    component: Register
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Cart.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-4 col-md-2">
+          <img
+            src="https://i.natgeofe.com/n/3861de2a-04e6-45fd-aec8-02e7809f9d4e/02-cat-training-NationalGeographic_1484324_square.jpg"
+            width="100%"
+          />
+        </div>
+        <div class="col">
+          <h5>แมวไทยโบราณวิเชียรแท้</h5>
+          <div>เพศเมีย</div>
+          <div>อายุ 5 เดือน</div>
+          <div>ฉีดวัคซีน: ครบ</div>
+          <div>โรคประจำตัว: -</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
